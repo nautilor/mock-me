e5ca0a6bca880f9dfdd53f1df0d76eaaacbc8f19
Edoardo Zerbo <edoardo.zerbo@temera.it>
2022-06-07 10:30:46 +0200

    added README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mock Me
+
+This is a simple script i use to mock rest api
+
+## Usage
+
+install dependecies using `yarn install`
+
+define the routes in `routes/routes.js`
+
+starts with `yarn listen`
+
+enjoy
--- routes/routes.js
+++ routes/routes.js
@@ -13,43 +13,21 @@ const _underscore = require("underscore");
  * 
  * N.B.    --> for the response body you can define a function to execute where you could run some code 
  */
+
 const routes = [
     {
         path: '/welcome/:name',
         body: welcome,
         status: 200,
         method: "GET"
-    },
-    {
-        path: '/users/',
-        body: save_user,
-        status: 200,
-        method: "POST"
-    },
-    {
-        path: '/users/',
-        body: get_user,
-        status: 200,
-        method: "GET"
-    },
+    }
 ]
-
-let users = []
-
-async function save_user(route, req, res) {
-    user = req.json;
-    console.log(user);
-    users.push(user);
-    return res.status(route.status).json(users);
-}
-
-async function get_user(route, req, res) {
-    return res.status(route.status).json(users);
-}
 
 async function welcome(route, req, res) {
     name  = req.params.name
     return res.status(route.status).json({ "message": `Welcome ${name}!` });
 }
 
+
+// Leave this here for the routes to work
 exports.routes = routes;
